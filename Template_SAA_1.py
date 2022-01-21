9f86fe34a7048668d669968e15f47eab306d070c
byjusmx <87218747+byjusmx@users.noreply.github.com>
2022-01-21 14:34:52 -0600

    Update Template_SAA_1.py

--- Template_SAA_1.py
+++ Template_SAA_1.py
@@ -2,15 +2,15 @@ import turtle
 
 wn = turtle.Screen()
 
-wn.bgcolor()#Write the color as light green
+wn.bgcolor()#Escribe light green - verde claro, como el color
 
-turtle.color()#Write the color as blue
+turtle.color()#Escribe blue - azul, como el color
 
-def sqrfunc():#Pass the value size and speed
+def sqrfunc():#Pasa el valor de size y speed
     for i in range(4):
         (size, speed) = (size + 5, speed + 5)
-        (turtle.speed())#Use the variable speed
-        turtle.fd()#Use the variable name
+        (turtle.speed())#Utiliza la variable speed
+        turtle.fd()#Utiliza la variable name
         turtle.left(90)
 
 sqrfunc(6, 2)
@@ -19,5 +19,5 @@ sqrfunc(46, 7)
 sqrfunc(66, 10)
 sqrfunc(86, 15)
 sqrfunc(106, 20)
-sqrfunc()#Fill in the values as 116,30
-sqrfunc()#Fill in the values as 136,40
+sqrfunc()#Rellena con los valores 116,30
+sqrfunc()#Rellena con los valores 136,40
